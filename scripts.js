4bdb0a4ac756753491a1668518d362688564e29a
João Gabriel Motta <jgmotta2004@gmail.com>
2024-12-27 17:25:28 -0300

    Adicionado estilização nas listas

--- scripts.js
+++ scripts.js
@@ -1,11 +1,18 @@
 const input = document.getElementById("ipt")
 const list = document.getElementById("list")
 
+input.addEventListener("keypress", function (event) {
+  if (event.key === "Enter" && input.value !== "") {
+    itemAdd()
+  }
+})
+
 function itemAdd() {
   const itemText = input.value.trim()
   if (itemText) {
     const li = document.createElement("li")
-    li.innerHTML = `
+    li.innerHTML = ` 
+      <input type="checkbox" />
       <span>${itemText}</span>
       <button class="deleteButton" onclick="deleteItem(this)">Remover</button>
     `
